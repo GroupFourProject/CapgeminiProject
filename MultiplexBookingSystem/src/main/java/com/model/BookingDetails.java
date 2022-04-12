409865211942ef9f1bbe4118ba2a2abdb380f3ad
omkar-7888 <101782885+omkar-7888@users.noreply.github.com>
2022-04-12 10:52:09 +0530

    Booking Details
    .

---
+++ MultiplexBookingSystem/src/main/java/com/model/BookingDetails.java
@@ -0,0 +1,41 @@
+package com.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class BookingDetails {
+	@Id
+	@GeneratedValue
+	@Column(name = "bookingId")
+	private int bookingId;
+	@Column(name = "seatTypeId")
+	private int seatTypeId;
+	@Column(name = "noOfSeats")
+	private int noOfSeats;
+	
+	public BookingDetails() {
+		super();
+	}
+	
+	public int getBookingId() {
+		return bookingId;
+	}
+	public void setBookingId(int bookingId) {
+		this.bookingId = bookingId;
+	}
+	public int getSeatTypeId() {
+		return seatTypeId;
+	}
+	public void setSeatTypeId(int seatTypeId) {
+		this.seatTypeId = seatTypeId;
+	}
+	public int getNoOfSeats() {
+		return noOfSeats;
+	}
+	public void setNoOfSeats(int noOfSeats) {
+		this.noOfSeats = noOfSeats;
+	}
+}
